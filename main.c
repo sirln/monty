e251c6de2cbfa281be77903cb7c93f436947d478
amisoni70 <amisoni7014@gmail.com>
2023-06-22 05:01:11 -0700

    Monty

--- main.c
+++ main.c
@@ -1,11 +1,41 @@
 #include "monty.h"
 
-int main(int argc, __attribute__((unused)) char **argv)
+value_t value = {NULL, NULL, NULL};
+/**
+ * main - code interpretor for monty
+ * @argc: no. of arguements passed 
+ * @argv: file location for monty
+ * Return: 0 on success
+**/
+
+int main(int argc, char **argv)
 {
+	FILE *file;
+	char *content;
+	size_t size = 0;
+	ssize_t read = 1;
+	unsigned int count = 0;
+
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
+	file = fopen(argv[1], "r");
+	value.file = file;
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (read > 0)
+	{
+		content = NULL;
+		read = getline(&content, &size, file);
+		value.content = content;
+		count++;
+		free(content);
+	}
+	fclose(file);	
 	return (0);
 }
--- monty.h
+++ monty.h
@@ -1,8 +1,10 @@
 #ifndef MONTY_H
 #define MONTY_H
 
+#define _POSIX_C_SOURCE 200809L
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -34,6 +36,21 @@ typedef struct instruction_s
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+/**
+ * struct valid_s - Contains the args, ptr to monty file & content of the line
+ * @argument: value
+ * @file: pointer to monty file
+ * @content: line content
+**/
+
+typedef struct value_s
+{
+	char *argument;
+	FILE *file;
+	char *content;
+} value_t;
+extern value_t value;
 
 
 #endif
