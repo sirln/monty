ed736311e73f02a60cea5dd6699e0cd1260bc688
amisoni70 <amisoni7014@gmail.com>
2023-06-22 12:35:47 -0700

    monty opcodes

--- monty_opcodes.c
+++ monty_opcodes.c
@@ -27,20 +27,64 @@ void op_push(stack_t **stack, size_t line_number)
 	*stack = new_stack;
 }
 
-
 /**
-  *op_pall - prints all the elements of in a stack.
+  *op_pall - prints all the elements in a stack.
   *
   *@h: pointer to pointer to the top of the stack
   *
   */
 void op_pall(stack_t **h)
 {
-        if (!*h)
-                return (0);
+        if (*h == NULL)
+                return;
         while (*h)
         {
                 printf("%i\n", (*h)->n);
                 *h = (*h)->next;
         }
+}
+
+/**
+ * op_pint - Prints the value at the top of the stack
+ * @h: poniter to pointer to the top of stack
+ * @count: the line number
+**/
+void op_pint(stack_t **h, unsigned int count)
+{
+	if (*h == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", count);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*h)->n);
+}
+
+/**
+ * op_pop - Removes the top element of the stack
+ * @h: pointer to pointer to the top of stack
+ * @count: the line number
+**/
+void op_pop(stack_t **h, unsigned int count)
+{
+	stack_t *temp;
+
+	if (*h == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", count);
+		exit(EXIT_FAILURE);
+	}
+	temp = *h;
+	*h = temp->next;
+	free(temp);
+}
+
+/**
+ * op_nop - dosen't do anything
+ * @h: pointer to pointer to the top of stack
+ * @count: the line number
+**/
+void op_nop(stack_t **h, unsigned int count)
+{
+	(void) count;
+	(void) h;
 }
