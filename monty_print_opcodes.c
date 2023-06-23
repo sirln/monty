c6d48d9af9bd2fdc0a8ce0e31e0f21cf0353458d
sirln <siro@sirlawren.com>
2023-06-23 02:32:21 +0000

    Monty Project: monty header file, opcode functions, monty mainfunction and monty get opcode

--- monty_print_opcodes.c
+++ monty_print_opcodes.c
@@ -28,7 +28,7 @@ void op_pall(stack_t **stack, unsigned int line_number)
 **/
 void op_pint(stack_t **stack, unsigned int line_number)
 {
-	if (*stack == NULL)
+	if (!(*stack)||(*stack)->next == NULL )
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
